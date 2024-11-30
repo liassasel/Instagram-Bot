3ea3b869f14be228c40cc3f40bf0901070ddc29d
liassasel <auseche80@gmail.com>
2024-11-30 16:40:34 -0400

    1.1

--- main.py
+++ main.py
@@ -2,53 +2,134 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementClickInterceptedException
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-USERNAME = os.getenv('USERNAME')
-PASSWORD = os.getenv('PASSWORD')
+USERNAME = os.getenv("USERNAMEE")
+PASSWORD = os.getenv("PASSWORD")
+MAX_TIME_LOAD = 20
+MIN_TIME_LOAD = 2
+
+xpath = {
+    "decline_cookies": "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]",
+    "save_login_not_now_button": "//div[contains(text(), 'Ahora no')]",
+    "notification_not_now_button": "//button[contains(text(), 'Ahora no')]",
+    "like_button": "//*[@aria-label='Me gusta']",
+    "modal": "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[1]/section/main/div/header/section[3]/ul/li[2]/div/a",
+    "cancel_unfollow_button": "//button[contains(text(), 'Cancel')]",
+}
+
+css_selector = {
+    "follows_buttons": "._aano button"
+}
+
+urls = {
+    "login": "https://www.instagram.com/accounts/login/",
+    "post": "https://www.instagram.com/p/CzZQt6BIsck/"
+}
+
 
 class InstaFollower:
+
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=chrome_options)
 
+    def close_browser(self):
+        self.driver.close()
+
     def login(self):
-        url = 'https://www.instagram.com/accounts/login/'
-        self.driver.get(url)
-        time.sleep(4)
-        
-        #Check if the cookie warning is present on te page
-        decline_cookies_xpath = '"/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]'
-        cookie_warning = self.driver.find_elements(By.XPATH, decline_cookies_xpath)
-        if cookie_warning:
-            # Demiss the cookie warning by clicking an element or button
+        self.driver.get(urls["login"])
+
+        try:
+            cookie_warning = WebDriverWait(self.driver, MIN_TIME_LOAD).until(
+                EC.presence_of_element_located((By.XPATH, xpath["decline_cookies"]))
+            )
+            # Dismiss the cookie warning by clicking an element or button
             cookie_warning[0].click()
-        
-        username = self.driver.find_element(by=By.NAME, value='username')
-        password = self.driver.find_elements(by=By.NAME, value='password')
-        
+        except:
+            pass
+
+        username = self.driver.find_element(by=By.NAME, value="username")
+        password = self.driver.find_element(by=By.NAME, value="password")
+
         username.send_keys(USERNAME)
         password.send_keys(PASSWORD)
-        
-        time.sleep(2)
-        password.send_key(Keys.ENTER)
-        
-        time.sleep(4)
-        # Click 'Not now' and ignore Save-login inf
-        save_login_prompt = self.driver.find_element(by=By.XPATH, value='// button[contains(text(), "Ahora no")]')
-        if save_login_prompt:
+
+        WebDriverWait(self.driver, MIN_TIME_LOAD)
+        password.send_keys(Keys.ENTER)
+
+        try:
+            # Click "Not now" and ignore Save-login info prompt
+            save_login_prompt = WebDriverWait(self.driver, MIN_TIME_LOAD).until(
+                EC.presence_of_element_located((By.XPATH, xpath["save_login_not_now_button"]))
+            )
             save_login_prompt.click()
-            
-        time.sleep(3)
-        notifications_prompt = self.driver.find_element(by=By.XPATH, value='// button[contains(text(), "Ahora no")]')
-        if notifications_prompt:
+        except:
+            pass
+
+        try:
+            # Click "not now" on notifications prompt
+            notifications_prompt = WebDriverWait(self.driver, MIN_TIME_LOAD).until(
+                EC.presence_of_element_located((By.XPATH, xpath["notification_not_now_button"]))
+            )
             notifications_prompt.click()
+        except:
+            pass
+
+    def like_to_post(self):
+        self.driver.get(xpath["post"])
+
+        # Click like
+        like_button = WebDriverWait(self.driver, MAX_TIME_LOAD).until(
+        EC.element_to_be_clickable((By.XPATH, xpath["like_button"]))
+        )
+        like_button.click()
+        print("Like dado")
+        
+        # Acount navigate
+    def find_account(self, account_name):
+        WebDriverWait(self.driver, MIN_TIME_LOAD)
+
+        # Show followers of the selected account. 
+        self.driver.get(f"https://www.instagram.com/{account_name}")
+
+    
+    def find_followers(self, account_name):
+        WebDriverWait(self.driver, MIN_TIME_LOAD)
+
+        # Show followers of the selected account. 
+        self.driver.get(f"https://www.instagram.com/{account_name}/followers/")
+
+        # The xpath of the modal that shows the followers will change over time. Update yours accordingly.
+        modal = WebDriverWait(self.driver, MAX_TIME_LOAD).until(
+            EC.presence_of_element_located((By.XPATH, xpath["modal"]))
+        )
+        for i in range(10):
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            WebDriverWait(self.driver, MIN_TIME_LOAD)
+
+
+    def follow(self):
+        # Check and update the (CSS) Selector for the "Follow" buttons as required. 
+        all_buttons = self.driver.find_elements(By.CSS_SELECTOR, value=css_selector["follows_buttons"])
+
+        for button in all_buttons:
+            try:
+                button.click()
+                WebDriverWait(self.driver, MIN_TIME_LOAD)
+
+            # Clicking button for someone who is already being followed will trigger dialog to Unfollow/Cancel
+            except ElementClickInterceptedException:
+                cancel_button = self.driver.find_element(by=By.XPATH, value=xpath["cancel_unfollow_button"])
+                cancel_button.click()     
 
 bot = InstaFollower()
 bot.login()
-
+bot.find_followers("5amoljen")
+bot.close_browser()
